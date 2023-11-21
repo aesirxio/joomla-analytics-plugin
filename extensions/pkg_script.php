ea4962ae309cbeb462bab835362deed217d97988
bao <nguyenbao10ht@gmail.com>
2023-11-21 10:26:04 +0700

    revert

--- extensions/pkg_script.php
+++ extensions/pkg_script.php
@@ -19,19 +19,6 @@ class pkg_aesirx_analyticsInstallerScript
 			return true;
 		}
 
-		// Run composer install command
-        $composerCommand = 'composer install -d ' . JPATH_ROOT . '/plugins/system/aesirx_analytics';
-        exec($composerCommand, $output, $returnCode);
-
-		var_dump($composerCommand); die;
-
-        if ($returnCode !== 0) {
-            // Handle error if needed
-            echo "Composer install failed. Error code: $returnCode";
-			return false;
-            // You may want to throw an exception or log the error
-        }
-
 		/** @var DatabaseDriver $db */
 		$db    = Factory::getContainer()->get(DatabaseInterface::class);
 		$query = $db->getQuery(true)
