1a274af9d34d4e427e088fd58061d6877da2fe6e
AesirX <accounts@aesirx.io>
2024-09-16 15:56:01 +0200

    add MySQL functions

--- extensions/plugins/system/aesirx_analytics/src/Mysql/GetAllEvents.php
+++ extensions/plugins/system/aesirx_analytics/src/Mysql/GetAllEvents.php
@@ -2,47 +2,49 @@
 
 
 use AesirxAnalytics\AesirxAnalyticsMysqlHelper;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Filter\InputFilter;
 
 Class AesirX_Analytics_Get_All_Events extends AesirxAnalyticsMysqlHelper
 {
     function aesirx_analytics_mysql_execute($params = [])
     {
-        global $wpdb;
+        // Get Joomla database object
+        $db = Factory::getDbo();
+        $inputFilter = InputFilter::getInstance();
 
         // Validate and sanitize each parameter in the $params array
         $validated_params = [];
         foreach ($params as $key => $value) {
-            $validated_params[$key] = sanitize_text_field($value);
+            $validated_params[$key] = $inputFilter->clean($value, 'STRING');
         }
 
-        $where_clause = [
-            "#__analytics_events.event_name = %s",
-            "#__analytics_events.event_type = %s",
-        ];
-
-        $bind = [
-            'visit',
-            'action'
+         // Where clauses and bindings
+         $where_clause = [
+            $db->quoteName('#__analytics_events.event_name') . ' = ' . $db->quote('visit'),
+            $db->quoteName('#__analytics_events.event_type') . ' = ' . $db->quote('action')
         ];
 
         parent::aesirx_analytics_add_filters($validated_params, $where_clause, $bind);
 
-        $sql =
-            "SELECT
-            DATE_FORMAT(start, '%%Y-%%m-%%d') as date,
-            COUNT(#__analytics_events.visitor_uuid) as visits,
-            COUNT(DISTINCT #__analytics_events.visitor_uuid) as unique_visits
-            from `#__analytics_events`
-            left join `#__analytics_visitors` on #__analytics_visitors.uuid = #__analytics_events.visitor_uuid 
-            WHERE " . implode(" AND ", $where_clause) .
-            " GROUP BY date";
+        // Build SQL query
+        $sql = $db->getQuery(true)->select([
+                "DATE_FORMAT(" . $db->quoteName('start') . ", '%Y-%m-%d') as date",
+                "COUNT(" . $db->quoteName('#__analytics_events.visitor_uuid') . ") as visits",
+                "COUNT(DISTINCT " . $db->quoteName('#__analytics_events.visitor_uuid') . ") as unique_visits"
+            ])
+            ->from($db->quoteName('#__analytics_events'))
+            ->leftJoin($db->quoteName('#__analytics_visitors') . ' ON ' . $db->quoteName('#__analytics_visitors.uuid') . ' = ' . $db->quoteName('#__analytics_events.visitor_uuid'))
+            ->where(implode(" AND ", $where_clause))
+            ->group($db->quoteName('date'));
 
-        $total_sql =
-            "SELECT
-            COUNT(DISTINCT DATE_FORMAT(start, '%%Y-%%m-%%d')) as total
-            from `#__analytics_events`
-            left join `#__analytics_visitors` on #__analytics_visitors.uuid = #__analytics_events.visitor_uuid
-            WHERE " . implode(" AND ", $where_clause);
+        // Total query
+        $total_sql = $db->getQuery(true)
+            ->select("COUNT(DISTINCT DATE_FORMAT(" . $db->quoteName('start') . ", '%Y-%m-%d')) as total")
+            ->from($db->quoteName('#__analytics_events'))
+            ->leftJoin($db->quoteName('#__analytics_visitors') . ' ON ' . $db->quoteName('#__analytics_visitors.uuid') . ' = ' . $db->quoteName('#__analytics_events.visitor_uuid'))
+            ->where(implode(" AND ", $where_clause));
 
         $sort = self::aesirx_analytics_add_sort($validated_params, ["date", "unique_visits", "visits"], "date");
 
