c7ed55742d7849c53544b4de204145e3c63181eb
AesirX <accounts@aesirx.io>
2024-09-17 15:33:49 +0200

    add MYSQ file 8

--- extensions/plugins/system/aesirx_analytics/src/Mysql/GetLiveVisitorsList.php
+++ extensions/plugins/system/aesirx_analytics/src/Mysql/GetLiveVisitorsList.php
@@ -9,10 +9,11 @@ Class AesirX_Analytics_Get_Live_Visitors_List extends AesirxAnalyticsMysqlHelper
     {
         $db = Factory::getDbo();
 
-        $where_clause = [
-            "#__analytics_flows.start = #__analytics_flows.end",
-            "#__analytics_flows.start >= NOW() - INTERVAL 30 MINUTE",
-            "#__analytics_visitors.device != 'bot'"
+         // Prepare the WHERE clauses
+         $where_clause = [
+            $db->quoteName('#__analytics_flows.start') . ' = ' . $db->quoteName('#__analytics_flows.end'),
+            $db->quoteName('#__analytics_flows.start') . ' >= NOW() - INTERVAL 30 MINUTE',
+            $db->quoteName('#__analytics_visitors.device') . ' != ' . $db->quote('bot')
         ];
         $bind = [];
 
@@ -23,24 +24,40 @@ Class AesirX_Analytics_Get_Live_Visitors_List extends AesirxAnalyticsMysqlHelper
 
         // filters where clause for events
 
-        $total_sql =
-            "SELECT COUNT(DISTINCT #__analytics_flows.uuid) as total
-            from `#__analytics_flows`
-            left join `#__analytics_visitors` on #__analytics_visitors.uuid = #__analytics_flows.visitor_uuid
-            left join `#__analytics_events` on #__analytics_events.flow_uuid = #__analytics_flows.uuid
-            WHERE " . implode(" AND ", $where_clause) .
-            " GROUP BY `#__analytics_flows`.`visitor_uuid`";
+        $total_sql = $db->getQuery(true)
+            ->select('COUNT(DISTINCT ' . $db->quoteName('#__analytics_flows.uuid') . ') AS total')
+            ->from($db->quoteName('#__analytics_flows'))
+            ->join('LEFT', $db->quoteName('#__analytics_visitors') . ' ON ' . $db->quoteName('#__analytics_visitors.uuid') . ' = ' . $db->quoteName('#__analytics_flows.visitor_uuid'))
+            ->join('LEFT', $db->quoteName('#__analytics_events') . ' ON ' . $db->quoteName('#__analytics_events.flow_uuid') . ' = ' . $db->quoteName('#__analytics_flows.uuid'))
+            ->where(implode(' AND ', $where_clause))
+            ->group($db->quoteName('#__analytics_flows.visitor_uuid'));
 
-        $sql =
-            "SELECT #__analytics_flows.*, ip, user_agent, device, browser_name, browser_name, browser_version, domain, lang, city, isp, country_name, country_code, geo_created_at, #__analytics_visitors.uuid AS visitor_uuid,
-            MAX(CASE WHEN #__analytics_event_attributes.name = 'sop_id' THEN #__analytics_event_attributes.value ELSE NULL END) AS sop_id, 
-            #__analytics_events.url AS url
-            from `#__analytics_flows`
-            left join `#__analytics_visitors` on #__analytics_visitors.uuid = #__analytics_flows.visitor_uuid
-            left join `#__analytics_events` on #__analytics_events.flow_uuid = #__analytics_flows.uuid
-            left join `#__analytics_event_attributes` on #__analytics_events.uuid = #__analytics_event_attributes.event_uuid
-            WHERE " . implode(" AND ", $where_clause) .
-            " GROUP BY `#__analytics_flows`.`visitor_uuid`";
+        // Main SQL query to fetch the flow data and event attributes
+        $sql = $db->getQuery(true)
+            ->select([
+                $db->quoteName('#__analytics_flows') . '.*',
+                $db->quoteName('ip'),
+                $db->quoteName('user_agent'),
+                $db->quoteName('device'),
+                $db->quoteName('browser_name'),
+                $db->quoteName('browser_version'),
+                $db->quoteName('domain'),
+                $db->quoteName('lang'),
+                $db->quoteName('city'),
+                $db->quoteName('isp'),
+                $db->quoteName('country_name'),
+                $db->quoteName('country_code'),
+                $db->quoteName('geo_created_at'),
+                $db->quoteName('#__analytics_visitors.uuid', 'visitor_uuid'),
+                'MAX(CASE WHEN ' . $db->quoteName('#__analytics_event_attributes.name') . ' = ' . $db->quote('sop_id') . ' THEN ' . $db->quoteName('#__analytics_event_attributes.value') . ' ELSE NULL END) AS sop_id',
+                $db->quoteName('#__analytics_events.url', 'url')
+            ])
+            ->from($db->quoteName('#__analytics_flows'))
+            ->join('LEFT', $db->quoteName('#__analytics_visitors') . ' ON ' . $db->quoteName('#__analytics_visitors.uuid') . ' = ' . $db->quoteName('#__analytics_flows.visitor_uuid'))
+            ->join('LEFT', $db->quoteName('#__analytics_events') . ' ON ' . $db->quoteName('#__analytics_events.flow_uuid') . ' = ' . $db->quoteName('#__analytics_flows.uuid'))
+            ->join('LEFT', $db->quoteName('#__analytics_event_attributes') . ' ON ' . $db->quoteName('#__analytics_events.uuid') . ' = ' . $db->quoteName('#__analytics_event_attributes.event_uuid'))
+            ->where(implode(' AND ', $where_clause))
+            ->group($db->quoteName('#__analytics_flows.visitor_uuid'));
 
         $sort = parent::aesirx_analytics_add_sort(
             $params,
@@ -62,18 +79,124 @@ Class AesirX_Analytics_Get_Live_Visitors_List extends AesirxAnalyticsMysqlHelper
         );
 
         if (!empty($sort)) {
-            $sql .= " ORDER BY " . implode(", ", $sort);
+            $sql->order(implode(', ', $sort));
         }
 
         $list_response = parent::aesirx_analytics_get_list($sql, $total_sql, $params, [], $bind);
 
-        if (is_wp_error($list_response)) {
+        if ($list_response instanceof Exception) {
             return $list_response;
         }
 
         $list = $list_response['collection'];
 
-        if (empty($list)) {
+        if (!empty($list)) {
+            $collection = [];
+
+            $ret = [];
+            $dirs = [];
+
+            $bind = array_map(function($e) {
+                return $e['uuid'];
+            }, $list);
+
+            $queryEvents = $db->getQuery(true)
+                ->select('*')
+                ->from($db->quoteName('#__analytics_events'))
+                ->where($db->quoteName('flow_uuid') . ' IN (' . implode(',', array_fill(0, count($bind), $db->quote(''))) . ')');
+
+            // Set the query and execute it to get events
+            $db->setQuery($queryEvents);
+            $events = $db->loadObjectList();
+
+            $queryAttributes = $db->getQuery(true)
+                ->select('*')
+                ->from($db->quoteName('#__analytics_event_attributes', 'attr'))
+                ->join('LEFT', $db->quoteName('#__analytics_events', 'evt') . ' ON ' . $db->quoteName('evt.uuid') . ' = ' . $db->quoteName('attr.event_uuid'))
+                ->where($db->quoteName('evt.flow_uuid') . ' IN (' . implode(',', array_fill(0, count($bind), $db->quote(''))) . ')');
+
+            // Set the query
+            $db->setQuery($queryAttributes);
+
+            // Execute the query and get the results
+            $attributes = $db->loadObjectList(); 
+            
+            $hash_attributes = [];
+
+            foreach ($attributes as $second) {
+                $attr = (object)[
+                    'name' => $second->name,
+                    'value' => $second->value,
+                ];
+                if (!isset($hash_attributes[$second->event_uuid])) {
+                    $hash_attributes[$second->event_uuid] = [$attr];
+                } else {
+                    $hash_attributes[$second->event_uuid][] = $attr;
+                }
+            }
+
+            $hash_map = [];
+
+            foreach ($events as $second) {
+                $visitor_event = [
+                    'uuid' => $second->uuid,
+                    'visitor_uuid' => $second->visitor_uuid,
+                    'flow_uuid' => $second->flow_uuid,
+                    'url' => $second->url,
+                    'referer' => $second->referer,
+                    'start' => $second->start,
+                    'end' => $second->end,
+                    'event_name' => $second->event_name,
+                    'event_type' => $second->event_type,
+                    'attributes' => $hash_attributes[$second->uuid] ?? [],
+                ];
+
+                if (!isset($hash_map[$second->flow_uuid])) {
+                    $hash_map[$second->flow_uuid] = [$second->uuid => $visitor_event];
+                } else {
+                    $hash_map[$second->flow_uuid][$second->uuid] = $visitor_event;
+                }
+            }
+
+            foreach ($list as $item) {
+                $item = (object) $item;
+                
+                if (!empty($collection) && end($collection)['uuid'] == $item->uuid) {
+                    continue;
+                }
+
+                $geo = isset($item->geo_created_at) ? (object)[
+                    'country' => (object)[
+                        'name' => $item->country_name,
+                        'code' => $item->country_code,
+                    ],
+                    'city' => $item->city,
+                    'isp' => $item->isp,
+                    'created_at' => $item->geo_created_at,
+                ] : null;
+
+                $events = isset($hash_map[$item->uuid]) ? array_values($hash_map[$item->uuid]) : null;
+
+                $collection[] = [
+                    'uuid' => $item->uuid,
+                    'visitor_uuid' => $item->visitor_uuid,
+                    'ip' => $item->ip,
+                    'user_agent' => $item->user_agent,
+                    'device' => $item->device,
+                    'browser_name' => $item->browser_name,
+                    'browser_version' => $item->browser_version,
+                    'domain' => $item->domain,
+                    'lang' => $item->lang,
+                    'start' => $item->start,
+                    'end' => $item->end,
+                    'geo' => $geo,
+                    'events' => $events,
+                    'url' => $item->url,
+                    'sop_id' => $item->sop_id,
+                ];
+            }
+            
+        } else {
             return [
                 'collection' => [],
                 'page' => 1,
@@ -82,112 +205,6 @@ Class AesirX_Analytics_Get_Live_Visitors_List extends AesirxAnalyticsMysqlHelper
                 'total_elements' => 0,
             ];
         }
-
-        $collection = [];
-
-        $ret = [];
-        $dirs = [];
-
-        $bind = array_map(function($e) {
-            return $e['uuid'];
-        }, $list);
-
-        // doing direct database calls to custom tables
-        // placeholders depends one number of $bind
-        $events = $wpdb->get_results( // phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery, WordPress.DB.DirectDatabaseQuery.NoCaching
-            $wpdb->prepare(
-                "SELECT * FROM {$wpdb->prefix}analytics_events WHERE flow_uuid IN (" . implode(', ', array_fill(0, count($bind), '%s')) . ")", 
-                ...$bind
-            )
-        );
-
-        // doing direct database calls to custom tables
-        // placeholders depends one number of $bind
-        $attributes = $wpdb->get_results( // phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery, WordPress.DB.DirectDatabaseQuery.NoCaching
-            $wpdb->prepare(
-                "SELECT * FROM {$wpdb->prefix}analytics_event_attributes 
-                LEFT JOIN {$wpdb->prefix}analytics_events
-                ON {$wpdb->prefix}analytics_events.uuid = {$wpdb->prefix}analytics_event_attributes.event_uuid 
-                WHERE {$wpdb->prefix}analytics_events.flow_uuid IN (" . implode(', ', array_fill(0, count($bind), '%s')) . ")",
-                ...$bind
-            )
-        ); 
-        
-        $hash_attributes = [];
-
-        foreach ($attributes as $second) {
-            $attr = (object)[
-                'name' => $second->name,
-                'value' => $second->value,
-            ];
-            if (!isset($hash_attributes[$second->event_uuid])) {
-                $hash_attributes[$second->event_uuid] = [$attr];
-            } else {
-                $hash_attributes[$second->event_uuid][] = $attr;
-            }
-        }
-
-        $hash_map = [];
-
-        foreach ($events as $second) {
-            $visitor_event = [
-                'uuid' => $second->uuid,
-                'visitor_uuid' => $second->visitor_uuid,
-                'flow_uuid' => $second->flow_uuid,
-                'url' => $second->url,
-                'referer' => $second->referer,
-                'start' => $second->start,
-                'end' => $second->end,
-                'event_name' => $second->event_name,
-                'event_type' => $second->event_type,
-                'attributes' => $hash_attributes[$second->uuid] ?? [],
-            ];
-
-            if (!isset($hash_map[$second->flow_uuid])) {
-                $hash_map[$second->flow_uuid] = [$second->uuid => $visitor_event];
-            } else {
-                $hash_map[$second->flow_uuid][$second->uuid] = $visitor_event;
-            }
-        }
-
-        foreach ($list as $item) {
-            $item = (object) $item;
-            
-            if (!empty($collection) && end($collection)['uuid'] == $item->uuid) {
-                continue;
-            }
-
-            $geo = isset($item->geo_created_at) ? (object)[
-                'country' => (object)[
-                    'name' => $item->country_name,
-                    'code' => $item->country_code,
-                ],
-                'city' => $item->city,
-                'isp' => $item->isp,
-                'created_at' => $item->geo_created_at,
-            ] : null;
-
-            $events = isset($hash_map[$item->uuid]) ? array_values($hash_map[$item->uuid]) : null;
-
-            $collection[] = [
-                'uuid' => $item->uuid,
-                'visitor_uuid' => $item->visitor_uuid,
-                'ip' => $item->ip,
-                'user_agent' => $item->user_agent,
-                'device' => $item->device,
-                'browser_name' => $item->browser_name,
-                'browser_version' => $item->browser_version,
-                'domain' => $item->domain,
-                'lang' => $item->lang,
-                'start' => $item->start,
-                'end' => $item->end,
-                'geo' => $geo,
-                'events' => $events,
-                'url' => $item->url,
-                'sop_id' => $item->sop_id,
-            ];
-        }
-
 
         return [
             'collection' => $collection,
