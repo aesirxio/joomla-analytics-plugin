13cde630dfd1153fdc72dad8936f716b4c1c191a
AesirX <accounts@aesirx.io>
2024-09-17 12:35:54 +0200

    add MYSQ file 4

--- extensions/plugins/system/aesirx_analytics/src/Mysql/GetConversionProductChart.php
+++ extensions/plugins/system/aesirx_analytics/src/Mysql/GetConversionProductChart.php
@@ -8,34 +8,36 @@ Class AesirX_Analytics_Get_Conversion_Product_Chart extends AesirxAnalyticsMysql
     function aesirx_analytics_mysql_execute($params = [])
     {
         $db = Factory::getDbo();
-        
+
         $where_clause = [];
         $bind = [];
 
         parent::aesirx_analytics_add_conversion_filters($params, $where_clause, $bind);
 
-        $sql =
-            "SELECT
-            DATE_FORMAT(start, '%%Y-%%m-%%d') as date,
-            SUM(quantity) DIV 1 as quantity
-            from `#__analytics_conversion`
-            left join `#__analytics_conversion_item` on #__analytics_conversion.uuid = #__analytics_conversion_item.conversion_uuid
-            left join `#__analytics_flows` on #__analytics_conversion.flow_uuid = #__analytics_flows.uuid
-            WHERE " . implode(" AND ", $where_clause) .
-            " GROUP BY date";
+         // Main SQL query to fetch data
+        $sql = $db->getQuery(true)
+            ->select([
+                "DATE_FORMAT(" . $db->quoteName('start') . ", '%Y-%m-%d') AS " . $db->quoteName('date'),
+                "SUM(" . $db->quoteName('quantity') . ") DIV 1 AS " . $db->quoteName('quantity')
+            ])
+            ->from($db->quoteName('#__analytics_conversion'))
+            ->join('LEFT', $db->quoteName('#__analytics_conversion_item') . ' ON ' . $db->quoteName('#__analytics_conversion.uuid') . ' = ' . $db->quoteName('#__analytics_conversion_item.conversion_uuid'))
+            ->join('LEFT', $db->quoteName('#__analytics_flows') . ' ON ' . $db->quoteName('#__analytics_conversion.flow_uuid') . ' = ' . $db->quoteName('#__analytics_flows.uuid'))
+            ->where(implode(' AND ', $where_clause))
+            ->group( $db->quoteName('date'));
 
-        $total_sql =
-            "SELECT
-            COUNT(DISTINCT DATE_FORMAT(start, '%%Y-%%m-%%d')) as total
-            from `#__analytics_conversion`
-            left join `#__analytics_conversion_item` on #__analytics_conversion.uuid = #__analytics_conversion_item.conversion_uuid
-            left join `#__analytics_flows` on #__analytics_conversion.flow_uuid = #__analytics_flows.uuid
-            WHERE " . implode(" AND ", $where_clause);
+        // Total count query to count distinct dates
+        $total_sql = $db->getQuery(true)
+            ->select("COUNT(DISTINCT DATE_FORMAT(" . $db->quoteName('start') . ", '%Y-%m-%d')) AS total")
+            ->from($db->quoteName('#__analytics_conversion'))
+            ->join('LEFT', $db->quoteName('#__analytics_conversion_item') . ' ON ' . $db->quoteName('#__analytics_conversion.uuid') . ' = ' . $db->quoteName('#__analytics_conversion_item.conversion_uuid'))
+            ->join('LEFT', $db->quoteName('#__analytics_flows') . ' ON ' . $db->quoteName('#__analytics_conversion.flow_uuid') . ' = ' . $db->quoteName('#__analytics_flows.uuid'))
+            ->where(implode(' AND ', $where_clause));
 
         $sort = self::aesirx_analytics_add_sort($params, ["date", "quantity"], "date");
 
         if (!empty($sort)) {
-            $sql .= " ORDER BY " . implode(", ", $sort);
+            $sql->order(implode(', ', $sort));
         }
         
         return parent::aesirx_analytics_get_list($sql, $total_sql, $params, [], $bind);
