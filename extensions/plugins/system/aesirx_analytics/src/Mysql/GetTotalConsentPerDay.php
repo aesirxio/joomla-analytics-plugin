033c58c1de203a09b7c1e62d1af0ac5579de5857
AesirX <accounts@aesirx.io>
2024-09-18 13:40:13 +0200

    new function in Mysql 2

--- extensions/plugins/system/aesirx_analytics/src/Mysql/GetTotalConsentPerDay.php
+++ extensions/plugins/system/aesirx_analytics/src/Mysql/GetTotalConsentPerDay.php
@@ -8,32 +8,34 @@ Class AesirX_Analytics_Get_Total_Consent_Per_Day extends AesirxAnalyticsMysqlHel
     function aesirx_analytics_mysql_execute($params = [])
     {
         $db = Factory::getDbo();
-        
+
         $where_clause = [];
         $bind = [];
 
         parent::aesirx_analytics_add_consent_filters($params, $where_clause, $bind);
 
-        $sql =
-            "SELECT 
-            ROUND(COUNT(visitor_consent.uuid) / 2) AS total,
-            DATE_FORMAT(visitor_consent.datetime, '%%Y-%%m-%%d') as `date`
-            FROM `#__analytics_visitor_consent` AS visitor_consent
-            LEFT JOIN `#__analytics_visitors` AS visitors ON visitors.uuid = visitor_consent.visitor_uuid
-            WHERE " . implode(" AND ", $where_clause) .
-            " GROUP BY `date`";
+        // Build the SQL query for retrieving the total count and date
+        $sql = $db->getQuery(true)
+            ->select([
+                "ROUND(COUNT(" . $db->quoteName('visitor_consent.uuid') . ") / 2) AS total",
+                "DATE_FORMAT(" . $db->quoteName('visitor_consent.datetime') . ", '%Y-%m-%d') AS `date`"
+            ])
+            ->from($db->quoteName('#__analytics_visitor_consent', 'visitor_consent'))
+            ->leftJoin($db->quoteName('#__analytics_visitors', 'visitors') . ' ON ' . $db->quoteName('visitors.uuid') . ' = ' . $db->quoteName('visitor_consent.visitor_uuid'))
+            ->where(implode(" AND ", $where_clause))
+            ->group($db->quoteName('date'));
 
-        $total_sql =
-            "SELECT
-            COUNT(DISTINCT DATE_FORMAT(visitor_consent.datetime, '%%Y-%%m-%%d')) as total
-            FROM `#__analytics_visitor_consent` AS visitor_consent
-            LEFT JOIN `#__analytics_visitors` AS visitors ON visitors.uuid = visitor_consent.visitor_uuid
-            WHERE " . implode(" AND ", $where_clause);
+        // Build the total SQL query to get the total number of distinct dates
+        $total_sql = $db->getQuery(true)
+            ->select("COUNT(DISTINCT DATE_FORMAT(" . $db->quoteName('visitor_consent.datetime') . ", '%Y-%m-%d')) AS total")
+            ->from($db->quoteName('#__analytics_visitor_consent', 'visitor_consent'))
+            ->leftJoin($db->quoteName('#__analytics_visitors', 'visitors') . ' ON ' . $db->quoteName('visitors.uuid') . ' = ' . $db->quoteName('visitor_consent.visitor_uuid'))
+            ->where(implode(" AND ", $where_clause));
 
         $sort = self::aesirx_analytics_add_sort($params, ["date", "total"], "date");
 
         if (!empty($sort)) {
-            $sql .= " ORDER BY " . implode(", ", $sort);
+            $sql->order(implode(", ", $sort));
         }
 
         return parent::aesirx_analytics_get_list($sql, $total_sql, $params, [], $bind);
