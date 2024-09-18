aa01d1b96aa9b3efb3b272aa09809a15c01a768b
AesirX <accounts@aesirx.io>
2024-09-18 19:51:46 +0200

    new function in Mysql 6

--- extensions/plugins/system/aesirx_analytics/src/Mysql/RevokeConsentLevel1.php
+++ extensions/plugins/system/aesirx_analytics/src/Mysql/RevokeConsentLevel1.php
@@ -10,46 +10,63 @@ Class AesirX_Analytics_Revoke_Consent_Level1 extends AesirxAnalyticsMysqlHelper
     {
         $db = Factory::getDbo();
         $inputFilter = InputFilter::getInstance();
-        
+
         // Validate and sanitize each parameter in the $params array
         $validated_params = [];
         foreach ($params as $key => $value) {
-            $validated_params[$key] = sanitize_text_field($value);
+            $validated_params[$key] = $inputFilter->clean($value, 'STRING');
         }
-
-        global $wpdb;
 
         $expiration = gmdate('Y-m-d H:i:s');
         $visitor_uuid = $validated_params['visitor_uuid'];
 
-        // Execute the update
-        // doing direct database calls to custom tables
-        $wpdb->update( // phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery, WordPress.DB.DirectDatabaseQuery.NoCaching
-            $wpdb->prefix . 'analytics_visitor_consent',
-            ['expiration' => $expiration],
-            ['visitor_uuid' => $visitor_uuid, 'consent_uuid' => null, 'expiration' => null],
-            array('%s'),  // Data type for 'expiration'
-            array('%s')   // Data type for 'visitor_uuid'
-        );
+        // Prepare and execute the first update query for the #__analytics_visitor_consent table
+        $query = $db->getQuery(true);
+        $fields = [
+            $db->quoteName('expiration') . ' = ' . $db->quote($expiration)
+        ];
+        $conditions = [
+            $db->quoteName('visitor_uuid') . ' = ' . $db->quote($visitor_uuid),
+            $db->quoteName('consent_uuid') . ' IS NULL',
+            $db->quoteName('expiration') . ' IS NULL'
+        ];
 
-        // Execute the update
-        // doing direct database calls to custom tables
-        $wpdb->update( // phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery, WordPress.DB.DirectDatabaseQuery.NoCaching
-            $wpdb->prefix . 'analytics_visitors',
-            [
-                'ip' => '',
-                'lang' => '',
-                'browser_version' => '',
-                'browser_name' => '',
-                'device' => '',
-                'user_agent' => ''
-            ],
-            ['uuid' => $visitor_uuid],
-        );
+        $query->update($db->quoteName('#__analytics_visitor_consent'))
+            ->set($fields)
+            ->where($conditions);
 
-        if ($wpdb->last_error) {
-            error_log('Query error: ' . $wpdb->last_error);
-            return new WP_Error($wpdb->last_error);
+        try {
+            $db->setQuery($query);
+            $db->execute();
+        } catch (Exception $e) {
+            Factory::getApplication()->enqueueMessage('Database update error: ' . $e->getMessage(), 'error');
+            return false;
+        }
+
+        // Prepare and execute the second update query for the #__analytics_visitors table
+        $query = $db->getQuery(true);
+        $fields = [
+            $db->quoteName('ip') . ' = ""',
+            $db->quoteName('lang') . ' = ""',
+            $db->quoteName('browser_version') . ' = ""',
+            $db->quoteName('browser_name') . ' = ""',
+            $db->quoteName('device') . ' = ""',
+            $db->quoteName('user_agent') . ' = ""'
+        ];
+        $conditions = [
+            $db->quoteName('uuid') . ' = ' . $db->quote($visitor_uuid)
+        ];
+
+        $query->update($db->quoteName('#__analytics_visitors'))
+            ->set($fields)
+            ->where($conditions);
+
+        try {
+            $db->setQuery($query);
+            $db->execute();
+        } catch (Exception $e) {
+            Factory::getApplication()->enqueueMessage('Database update error: ' . $e->getMessage(), 'error');
+            return false;
         }
         
         return true;
